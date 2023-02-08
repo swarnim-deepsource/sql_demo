afadde5b29598f7ced787040f3856249fa856103
Swarnim Arun <99165284+swarnim-deepsource@users.noreply.github.com>
2023-02-08 16:59:23 +0530

    test: dev marvin-sql

--- test.sql
+++ test.sql
@@ -1,1 +1,17 @@
 SELECT name as foo, id as foo FROM table;
+
+SELECT
+     name,
+     age
+FROM students
+
+SELECT
+    name ,
+    age
+FROM students
+
+SELECT
+    c,
+    a +
+    b AS ab
+FROM students
